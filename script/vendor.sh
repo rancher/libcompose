2348815c0d35f9fbebbcfe7a7bea8609c1046682
Darren Shepherd <darren@rancher.com>
2016-05-30 22:48:48 -0700

    Update vendor

--- script/vendor.sh
+++ script/vendor.sh
@@ -10,10 +10,10 @@ clone git github.com/Sirupsen/logrus v0.9.0
 clone git github.com/codegangsta/cli 839f07bfe4819fa1434fa907d0804ce6ec45a5df
 clone git github.com/docker/distribution 467fc068d88aa6610691b7f1a677271a3fac4aac
 clone git github.com/vbatts/tar-split v0.9.11
-clone git github.com/docker/docker 9837ec4da53f15f9120d53a6e1517491ba8b0261
+clone git github.com/docker/docker v1.11.1
 clone git github.com/docker/go-units 651fc226e7441360384da338d0fd37f2440ffbe3
 clone git github.com/docker/go-connections v0.2.0
-clone git github.com/docker/engine-api fd7f99d354831e7e809386087e7ec3129fdb1520
+clone git github.com/docker/engine-api v0.3.3
 clone git github.com/flynn/go-shlex 3f9db97f856818214da2e1057f8ad84803971cff
 clone git github.com/gorilla/context 14f550f51a
 clone git github.com/gorilla/mux e444e69cbd

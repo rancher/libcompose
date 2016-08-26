ce2060d11a6e6e6bc36b2d219eafc0716d917744
Josh Curl <josh@curl.me>
2016-08-26 14:23:15 -0700

    Don't delete only stopped containers
    Signed-off-by: Josh Curl <josh@curl.me>

--- cli/app/app.go
+++ cli/app/app.go
@@ -239,27 +239,6 @@ func ProjectDelete(p project.APIProject, c *cli.Context) error {
 	options := options.Delete{
 		RemoveVolume: c.Bool("v"),
 	}
-	if !c.Bool("force") {
-		stoppedContainers, err := p.Containers(context.Background(), project.Filter{
-			State: project.Stopped,
-		}, c.Args()...)
-		if err != nil {
-			return cli.NewExitError(err.Error(), 1)
-		}
-		if len(stoppedContainers) == 0 {
-			fmt.Println("No stopped containers")
-			return nil
-		}
-		fmt.Printf("Going to remove %v\nAre you sure? [yN]\n", strings.Join(stoppedContainers, ", "))
-		var answer string
-		_, err = fmt.Scanln(&answer)
-		if err != nil {
-			return cli.NewExitError(err.Error(), 1)
-		}
-		if answer != "y" && answer != "Y" {
-			return nil
-		}
-	}
 	err := p.Delete(context.Background(), options, c.Args()...)
 	if err != nil {
 		return cli.NewExitError(err.Error(), 1)
